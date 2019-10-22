ee584ad8b9f3d243dbcf9c79594e28eee5a25ee3
Damian <Damian@10.20.58.214>
2019-10-22 14:06:17 +0200

    polo sur

--- Polo sur/src/polo_Sur.java
+++ Polo sur/src/polo_Sur.java
@@ -15,5 +15,15 @@ import java.util.ArrayList;
 public class polo_Sur {
     
     ArrayList<serVivo> animales = new ArrayList<serVivo>();
-    
+    public void creaNuestroPolo(){
+        
+    }
+    public void creaEsquimal(int min, int max){
+        int n, i;                                               //en n ira el numero de esquimales que abra al principio, i es solo para explorar el for
+        n = (int) Math.floor(Math.random()*(max-min+1)+min);    //metemos el numero de esquimales iniciales en n
+        for(i=0;i<=n ;i++){
+            //HAY QUE CREAR EL ESQUIMAL
+            //Y DESPUÉS LO METEMOS EN ANIMALES
+        }
+    }
 }
