128c4849246fa5f1c0d2687642a6bd076eee623d
joest <joest@10.20.43.67>
2019-10-22 12:48:18 +0200

    testear

---
+++ Polo sur/src/polo/sur/polo_Sur.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polo.sur;
+
+/**
+ *
+ * @author joest
+ */
+public class polo_Sur {
+   
+    /*ArrayList<esquimal> esquemales = new ArrayList<esquimales>();
+    ArrayList<oso> osos = new ArrayList<oso>();
+    ArrayList<morsas> morsa = new ArrayList<morsa>();
+    ArrayList<pez> peces = new ArrayList<pez>();
+    ArrayList<> esquemales = new ArrayList<esquimales>();*/
+}
